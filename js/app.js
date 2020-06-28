44b8e20de015aebb4244c3b29aa3ae798cd73485
deokgoo <kkddgg1001@gmail.com>
2020-06-28 18:47:36 +0900

    add navBar click evt

--- js/app.js
+++ js/app.js
@@ -5,10 +5,17 @@ const navBarHeight = navBar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
   if(window.scrollY > navBarHeight) {
-    console.log(navBar);
     navBar.classList.add('navbar--dark');
   }else {
     navBar.classList.remove('navbar--dark');
   }
-  
+})
+
+const navBarMenu = document.querySelector('.navbar__menu');
+navBarMenu.addEventListener('click', (evt) => {
+  const { link } = evt.target.dataset;
+  if(!link) return;
+  const scrollTo = document.querySelector(link);
+  console.log(scrollTo);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 })
