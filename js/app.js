cbdaa9c05914d2e169d27f7017a22c2235768232
deokgoo <kkddgg1001@gmail.com>
2020-06-28 19:40:42 +0900

    home section transparent

--- js/app.js
+++ js/app.js
@@ -13,12 +13,21 @@ document.addEventListener('scroll', () => {
 
 const navBarMenu = document.querySelector('.navbar__menu');
 const contactMeBtn = document.querySelector('.home__contact');
-navBarMenu.addEventListener('click', scrollToTarget);
-contactMeBtn.addEventListener('click', scrollToTarget);
+navBarMenu.addEventListener('click', scrollHandler);
+contactMeBtn.addEventListener('click', scrollHandler);
 
-function scrollToTarget(evt) {
+function scrollHandler(evt) {
   const { link } = evt.target.dataset;
   if(!link) return;
   const scrollTo = document.querySelector(link);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
+
+// home smooth
+
+document.addEventListener('scroll', () => {
+  const home = document.querySelector('.home__container');
+  const homeHeight = home.getBoundingClientRect().height;
+
+  home.setAttribute('style', `opacity: ${1-window.scrollY/homeHeight}`)
+})
